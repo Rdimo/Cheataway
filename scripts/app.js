4b8654144028400f70d1ef141670ba548024ad9d
Rdimo <rasmus.nyblom@hotmail.com>
2021-09-15 21:35:18 +0200

    added cool title changer

--- scripts/app.js
+++ scripts/app.js
@@ -118,3 +118,9 @@ particlesJS('particle-js',
   }
 
 );
+(function titleScroller(text) {
+  document.title = text;
+  setTimeout(function () {
+    titleScroller(text.substr(1) + text.substr(0, 1));
+  }, 500);
+}("CheatAway-"));
