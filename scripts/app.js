5f2dbd0586e42a1ad0952d6514a63b365ad2c9f4
Rdimo <rasmus.nyblom@hotmail.com>
2021-11-23 18:56:42 +0100

    added glitched text effect + more stuff

--- scripts/app.js
+++ scripts/app.js
@@ -3,10 +3,10 @@ particlesJS('particle-js',
   {
     "particles": {
       "number": {
-        "value": 150,
+        "value": 200,
         "density": {
           "enable": true,
-          "value_area": 1200
+          "value_area": 1000
         }
       },
       "color": {
@@ -46,13 +46,6 @@ particlesJS('particle-js',
           "size_min": 0.1,
           "sync": false
         }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 0
       },
       "move": {
         "enable": true,
@@ -124,3 +117,104 @@ particlesJS('particle-js',
     titleScroller(text.substr(1) + text.substr(0, 1));
   }, 500);
 }("CheatAway-"));
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var TextScramble = function () {
+  function TextScramble(el) {
+    _classCallCheck(this, TextScramble);
+
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}@€$¤£%&”=+*^?#´\"\'________';
+    this.update = this.update.bind(this);
+  }
+
+  _createClass(TextScramble, [{
+    key: 'setText',
+    value: function setText(newText) {
+      var _this = this;
+
+      var oldText = this.el.innerText;
+      var length = Math.max(oldText.length, newText.length);
+      var promise = new Promise(function (resolve) {
+        return _this.resolve = resolve;
+      });
+      this.queue = [];
+      for (var i = 0; i < length; i++) {
+        var from = oldText[i] || '';
+        var to = newText[i] || '';
+        var min = 40,
+        max = 75;
+        var rand = Math.random() * (max - min) + min;
+        var start = Math.floor(Math.random() * rand);
+        var end = start + Math.floor(Math.random() * rand);
+        this.queue.push({ from: from, to: to, start: start, end: end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+    }
+  }, {
+    key: 'update',
+    value: function update() {
+      var output = '';
+      var complete = 0;
+      for (var i = 0, n = this.queue.length; i < n; i++) {
+        var _queue$i = this.queue[i],
+            from = _queue$i.from,
+            to = _queue$i.to,
+            start = _queue$i.start,
+            end = _queue$i.end,
+            char = _queue$i.char;
+
+        if (this.frame >= end) {
+          complete++;
+          output += to;
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar();
+            this.queue[i].char = char;
+          }
+          output += '<span class="dud">' + char + '</span>';
+        } else {
+          output += from;
+        }
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+        this.resolve();
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update);
+        this.frame++;
+      }
+    }
+  }, {
+    key: 'randomChar',
+    value: function randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+  }]);
+
+  return TextScramble;
+}();
+
+var phrases = ["Cheataway","Cheataway"];
+
+var el = document.querySelector('.glitch-text');
+var fx = new TextScramble(el);
+
+var counter = 0;
+
+var min = 1000, max = 5000;
+var rand = Math.random() * (max - min) + min;
+
+var next = function next() {
+  fx.setText(phrases[counter]).then(function () {
+    setTimeout(next, rand);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+next();
--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -5,33 +5,27 @@ var start = function() {
     var ctx = new AudioContext();
     var analyser = ctx.createAnalyser();
     var audioSrc = ctx.createMediaElementSource(audio);
-    // we have to connect the MediaElementSource with the analyser 
     audioSrc.connect(analyser);
     analyser.connect(ctx.destination);
-    // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
-    // analyser.fftSize = 64;
-    // frequencyBinCount tells you how many values you'll receive from the analyser
     
-    // we're ready to receive some data!
     var canvas = document.getElementById('canvas'),
         cwidth = canvas.width,
         cheight = canvas.height - 2,
-        meterWidth = 10, //width of the meters in the spectrum
-        gap = 2, //gap between meters
+        meterWidth = 10, 
+        gap = 2,
         capHeight = 2,
         capStyle = '#fff',
-        meterNum = 800 / (10 + 2), //count of the meters
-        capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+        meterNum = 800 / (10 + 2), 
+        capYPositionArray = [];
     ctx = canvas.getContext('2d'),
     gradient = ctx.createLinearGradient(0, 0, 0, 300);
     gradient.addColorStop(1, '#0f0');
     gradient.addColorStop(0.5, '#ff0');
     gradient.addColorStop(0, '#f00');
-    // loop
     function renderFrame() {
         var array = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var step = Math.round(array.length / meterNum); //sample limited data from the total array
+        var step = Math.round(array.length / meterNum);
         ctx.clearRect(0, 0, cwidth, cheight);
         for (var i = 0; i < meterNum; i++) {
             var value = array[i * step];
@@ -39,15 +33,14 @@ var start = function() {
                 capYPositionArray.push(value);
             };
             ctx.fillStyle = capStyle;
-            //draw the cap, with transition effect
             if (value < capYPositionArray[i]) {
                 ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
             } else {
                 ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
                 capYPositionArray[i] = value;
             };
-            ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+            ctx.fillStyle = gradient;
+            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight);
         }
         requestAnimationFrame(renderFrame);
     }
